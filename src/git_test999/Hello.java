4f1bbc3fb5c96d6b8a403384dc15aa79386c18d4
Seong, Hyunwoo <ssssww1135@gmail.com>
2022-09-26 03:07:07 +0900

    화면 출력 방법 수정

--- src/git_test999/Hello.java
+++ src/git_test999/Hello.java
@@ -4,8 +4,9 @@ public class Hello {
     
     String message = "안녕하세요";
 
-    public void say() {
-        System.out.println(message);
+    public String say() {
+        return message;
+
     }
 
 }
